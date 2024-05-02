18c35e59b996ae7afbf07de247f4b609e0cc3f86
AnastasiiaYablonska <0974406907nn@gmail.com>
2024-05-02 16:59:18 +0300

    hw_less2_draft

---
+++ hw_less2/hw1_1.js
@@ -0,0 +1,209 @@
+//Масиви та об'єкти:
+
+// Створити масив, наповнити його 10 елементами будь-якого типу.
+let array2 = [10, "hello", true, 3.14, null, undefined, false, { name: "Bob"}]
+
+// Вивести кожен елемент в консоль
+console.log(array2[0]);
+console.log(array2[1]);
+console.log(array2[2]);
+console.log(array2[3]);
+console.log(array2[4]);
+console.log(array2[5]);
+console.log(array2[6]);
+console.log(array2[7]);
+console.log(array2[8]);
+console.log(array2[9]);
+
+// - Створити 3 об'єкти які описують книги.
+// Поля об'єкту : title ,pageCount, genre.
+let book1 = {
+    title: "The Lord of the Rings",
+    pageCount: 1178,
+    genre: "Fantasy",
+}
+console.log(book1);
+
+let book2 = {
+    title: "The Diary of Anne Frank",
+    pageCount: 336,
+    genre: "Historical fiction",
+}
+let book3 = {
+    title: "Harry Potter and the Sorcerer's Stone",
+    pageCount: 265,
+    genre: "Children's fantasy",
+}
+
+console.log(book2);
+console.log(book3);
+
+//Створити 3 об'єкти які описують книги.
+// Поля об'єкту : title ,pageCount, genre, authors.
+// Поле "автори" - являється  масивом.
+// Кожен автор має поля name та age.
+
+let book4 = {
+    title: "To Kill a Mockingbird",
+    pageCount: 296,
+    genre: "Roman",
+    authors: ["Harper Lee"],
+}
+
+let book5 = {
+    title: "Pride and Prejudice",
+    pageCount: 234,
+    genre: "Romantic novel",
+    authors: ["Jane Austen"],
+}
+
+let book6 = {
+    title: "The Book Thief",
+    pageCount: 550,
+    genre: "Historical novel",
+    authors: ["Markus Zusak"],
+}
+console.log(book4);
+console.log(book5);
+console.log(book6);
+
+// - Створити масив з 10 об'єктами які описують сутніть "користувач".
+// Поля: name, username,password.
+// Вивести в консоль пароль кожного користувача
+let array= [
+    let.user1 = {
+        name: 'Jennifer',
+        surname: 'Petrovich',
+        lastname: 'Doe',
+        username: 'J1',
+        password: '123456Zz',
+        age: 20,
+        experience: 2,
+        position: 'manager',
+        address: '345 Park Avenue',
+        email: 'johnson@gmail.com'
+    },
+    let.user2 = {
+            name: 'James',
+            surname: 'Pavlovich',
+            lastname: 'Smith',
+            username: 'J2',
+            password: '123456ZXZx',
+            age: 25,
+            experience: 5,
+            position: 'manager',
+            address: '456 Elm St, Anothertown, NY 54321',
+            email: '123@gmail.com'
+    },
+    let.user3 = {
+            name: 'Jessica',
+            surname: 'Urievich',
+            lastname: 'Johnson',
+            username: 'J3',
+            password: '123456ASD',
+            age: 29,
+            experience: 3,
+            position: 'manager',
+            address: '789 Oak St, Sometown, TX 76543',
+            email: '4yun@gmail.com'
+    },
+    let.user4 = {
+            name: 'Bob',
+            surname: 'Petrovich',
+            lastname: 'Williams',
+            username: 'Bob1',
+            password: '123456ASDFCD',
+            age: 30,
+            experience: 1,
+            position: 'manager',
+            address: '1011 Maple St, Bigtown, IL 60543',
+            email: 'jon@gmail.com'
+    },
+    let.user5 = {
+            name: 'Sarah',
+            surname: "Olegovich",
+            lastname: 'Brown',
+            username: 'S1',
+            password: '123456Sdx',
+            age: 24,
+            experience: 2.5,
+            position: 'manager',
+            address: '1213 Pine St, Smalltown, PA 18901',
+            email: 'yyy5@gmail.com'
+    },
+    let.user6 = {
+            name: 'David',
+            surname: 'Petrovich',
+            lastname: 'Jones',
+            username: 'D111',
+            password: '123456SDXDC',
+            age: 35,
+            experience: 4.5,
+            position: 'manager',
+            address: '1415 Poplar St, Tinytown, OH 43210',
+            email: 'son@gmail.com'
+    },
+    let.user7 = {
+            name: 'Elizabeth',
+            surname: 'Petrovich',
+            lastname: 'Miller',
+            username: 'Elza',
+            password: '123456',
+            age: 28,
+            experience: 6,
+            position: 'manager',
+            address: '1617 Spruce St, Littletown, MO 65732',
+            email: 'hnson@gmail.com'
+    },
+    let.user8 = {
+            name: 'Robert',
+            surname: 'Petrovich',
+            lastname: 'Davis',
+            username: 'Rob',
+            password: '123456D',
+            age: 31,
+            experience: 2,
+            position: 'manager',
+            address: '1819 Willow St, Nowhere, AZ 85243',
+            email: 'onyy@gmail.com'
+    },
+    let.user9 = {
+            name: 'Mary',
+            surname: 'Petrovich',
+            lastname: 'Taylor',
+            username: 'MaryMary',
+            password: '123456',
+            age: 19,
+            experience: 7,
+            position: 'manager',
+            address: '2021 Cedar St, Somewhere, FL 33456',
+            email: 'tunson@gmail.com'
+    },
+    let.user10 =['Bob','Petrovich', 'Martin','BobMar5', '123456', 27, 5'manager','2223 Walnut St, Anywhere, USA 12345','hnstttt@gmail.com']
+    {
+            name: 'Bob',
+            surname: 'Petrovich',
+            lastname: 'Martin',
+            username: 'BobMar5',
+            password: '123456',
+            age: 27,
+            experience: 5,
+            position: 'manager',
+            address: '2223 Walnut St, Anywhere, USA 12345',
+            email: 'hnstttt@gmail.com'
+    },
+]
+
+console.log(array.length[4]);
+console.log(array.length[4]);
+console.log(array.length[4]);
+console.log(array.length[4]);
+console.log(array.length[4]);
+console.log(array.length[4]);
+console.log(array.length[4]);
+console.log(array.length[4]);
+console.log(array.length[4]);
+console.log(array.length[4]);
+
+
+
