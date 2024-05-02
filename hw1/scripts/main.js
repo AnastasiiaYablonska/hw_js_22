086fed6c265eedc54fb4aded530fc6e62ab46887
AnastasiiaYablonska <0974406907nn@gmail.com>
2024-05-02 09:54:45 +0300

    hw1/1 ok

---
+++ hw1/scripts/main.js
@@ -0,0 +1,27 @@
+let a='hello'
+console.log(a)
+let b='owu'
+console.log(b)
+let c='com'
+console.log(c)
+let d='ua'
+console.log(d)
+let e=1
+console.log(e)
+let f=10
+console.log(f)
+let z=-999
+console.log(z)
+let h=123
+console.log(h)
+let i=3.14
+console.log(i)
+let g=2.7
+console.log(g)
+let k=16
+console.log(k)
+let n=true
+console.log(n)
+let l=5>6
+console.log(l)
+
